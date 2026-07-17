29795b1a82158bd2b380405d28bdf75981f68b01
Nikita Nikiforov <nsn8@tpu.ru>
2026-07-17 19:04:45 +0700

    #6743852 | age specific scripts and styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,17 +2,10 @@
 <html lang="ru">
 <head>
     @vite(['resources/css/app.css'])
-    @vite(['resources/css/travels.css'])
-    @vite(['resources/css/travel.css'])
     @vite(['resources/css/components/modal.css'])
-    @vite(['resources/css/components/travel-element.css'])
-    @vite(['resources/css/components/document-element.css'])
     @vite(['resources/js/app.js'])
-    @vite(['resources/js/travels.js'])
-    @vite(['resources/js/travel.js'])
     @vite(['resources/js/components/modal.js'])
-    @vite(['resources/js/components/travel-element.js'])
-    @vite(['resources/js/components/document-element.js'])
+    @yield('styles')
     <meta charset="UTF-8">
     <title>@yield('title', 'Мой сайт')</title>
 </head>
@@ -21,6 +14,7 @@
 
     <div class="container">
         @yield('content')
+        @yield('scripts')
     </div>
 </body>
 </html>
